f9596e4708510a755c5fa76f504197d1efe53851
Fran <carrasco211993@gmail.com>
2023-03-23 12:02:10 +0100

    Add form // Add Validation Form

--- src/Controller/RegistroController.php
+++ src/Controller/RegistroController.php
@@ -2,19 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistroController extends AbstractController
 {
+    // Mi redirección
     /**
      * @Route("/registro", name="app_registro")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $user = new User();
+        // Add Form
+        $form = $this->createForm(UserType::class,$user);
+        // Add Entity Manager
+        $form->handleRequest($request);
+        // Submit and valid
+        if($form->isSubmitted() && $form->isValid()) {
+            // Set default props
+            $user->setBan(false);
+            $user->setRoles(['Role_user']);
+            // Manage Entity
+            $em = $this->getDoctrine()->getManage();
+            // Persist
+            $em->persist($user);
+            // Clear exit buffer
+            $em->flush();
+            // Add Message
+            $this->addFlash('success', 'Se añadió correctamente');
+            // Redirect
+            return $this->redirectToRoute('registro');
+        }
+        // Render
         return $this->render('registro/index.html.twig', [
             'controller_name' => 'RegistroController',
+            'formulario' => $form->createView()
         ]);
     }
 }
